fd1af262635e252b4edffdf076d6bd39b8c9fe9a
juliandescottes <julian.descottes@gmail.com>
2013-05-15 22:17:12 +0200

    Added HTML widget library to default libs

--- scripts/instantat.js
+++ scripts/instantat.js
@@ -5,103 +5,103 @@
 		editor = null, store = null, data_editor = null;
 
 	var snippet = { 
-        template : "{macro main()}\n    <ul>\n    {foreach fruit in data.fruits}\n        ${fruit}, test\n    {/foreach}\n    </ul>\n{/macro}" , 
-        script : "({\n    $classpath:'TestScript',\n    $prototype : {\n        myMethod : function () {\n\n        }\n    }\n})" , 
-        css : "{macro main()}\n    ul {\n        padding-left : 10px;\n        color:red;\n    }   \n{/macro}" , 
-        data : "var data = {\n    fruits : [\"Banana\", \"Orange\", \"Apple\"]\n}"
-    };
+	  template : "{macro main()}\n    <ul>\n    {foreach fruit in data.fruits}\n        ${fruit}, test\n    {/foreach}\n    </ul>\n{/macro}" , 
+	  script : "({\n    $classpath:'TestScript',\n    $prototype : {\n        myMethod : function () {\n\n        }\n    }\n})" , 
+	  css : "{macro main()}\n    ul {\n        padding-left : 10px;\n        color:red;\n    }   \n{/macro}" , 
+	  data : "var data = {\n    fruits : [\"Banana\", \"Orange\", \"Apple\"]\n}"
+	};
 
 	var updateEditorSilently = function (e, content) {
 		unplugged = true;
-		    e.getSession().setValue(content,0);
-	    unplugged = false;
-	}
+		e.getSession().setValue(content,0);
+	  unplugged = false;
+	};
 
 	var selectEditor = function (evt) {
 		var previousType = currentType;
-	    currentType = evt.target.innerHTML.toLowerCase(); 
+    currentType = evt.target.innerHTML.toLowerCase(); 
 
-	    updateEditorSilently(editor, snippet[currentType]);
+    updateEditorSilently(editor, snippet[currentType]);
 
-	    if (currentType == "template") editor.getSession().setMode("ace/mode/aria");
-	    if (currentType == "script") editor.getSession().setMode("ace/mode/javascript");
-	    if (currentType == "css") editor.getSession().setMode("ace/mode/aria");
+    if (currentType == "template") editor.getSession().setMode("ace/mode/aria");
+    if (currentType == "script") editor.getSession().setMode("ace/mode/javascript");
+    if (currentType == "css") editor.getSession().setMode("ace/mode/aria");
 
-	   	document.getElementById("tab-" + previousType).classList.remove("tab-selected");
-	    document.getElementById("tab-" + currentType).classList.add("tab-selected");
+   	document.getElementById("tab-" + previousType).classList.remove("tab-selected");
+    document.getElementById("tab-" + currentType).classList.add("tab-selected");
 
-	    errors.refreshErrors();
+    errors.refreshErrors();
 	};
 
 	var loadModel = function (model_content) {
-	    try {
-	        eval(model_content);  
+    try {
+      eval(model_content);  
 			errors.removeError("data");  
-	    } catch (e) {
+    } catch (e) {
 			errors.setError("data","[DATA MODEL ERROR] : " + e.message);
-	        var data = {};
-	    }
+      var data = {};
+    }
 
-	    return data;
+    return data;
 	};
 
 	var loadTemplate = function (tpl_content, data) {
-	    var tplString = "{Template {$classpath : 'Test', $hasScript:true, $css:['TestStyle']}}"+tpl_content+"{/Template}"
-	    aria.templates.TplClassGenerator.parseTemplate(tplString, false,
-	        {
-	            fn : function (res, args) {
-	                if (res.classDef) {
+    var tplString = "{Template {$classpath : 'Test', $hasScript:true, $css:['TestStyle']}}"+tpl_content+"{/Template}"
+    aria.templates.TplClassGenerator.parseTemplate(tplString, false,
+      {
+        fn : function (res, args) {
+          if (res.classDef) {
 						errors.removeError("template");
-	                    loadTemplateInPreview(res.classDef, data);
-	                } 
-	            }
-	        },{"file_classpath" : "Test"}
-	    );
+            loadTemplateInPreview(res.classDef, data);
+         	} 
+        }
+      },{"file_classpath" : "Test"}
+    );
 	};
 
 	var loadTemplateInPreview = function (classDef, data) {
-	    Aria["eval"](classDef); 
-	    Aria.loadTemplate({
-	        classpath: "Test",
-	        div: "preview",
-	        data: data
-	    });
+    Aria["eval"](classDef); 
+    Aria.loadTemplate({
+      classpath: "Test",
+      div: "preview",
+      data: data
+    });
 	};
 
 	var loadTemplateScript = function (script_content) {
-	    try {
-	        eval("Aria.tplScriptDefinition("+script_content+");");
+    try {
+      eval("Aria.tplScriptDefinition("+script_content+");");
 			errors.removeError("script");  
-	    } catch (e) {
+    } catch (e) {
 			errors.setError("script", "[SCRIPT ERROR] : " + e.message);
-	    }
+    }
 	};
 
 	var loadTemplateStyle = function (css_content, data) {
-	    var tplString = "{CSSTemplate {$classpath : 'TestStyle'}}"+css_content+"{/CSSTemplate}"
-	    aria.templates.CSSClassGenerator.parseTemplate(tplString, false,
-	        {
-	            fn : function (res, args) {
-	                if (res.classDef) {
+    var tplString = "{CSSTemplate {$classpath : 'TestStyle'}}"+css_content+"{/CSSTemplate}"
+    aria.templates.CSSClassGenerator.parseTemplate(tplString, false, 
+    	{
+        fn : function (res, args) {
+          if (res.classDef) {
 						errors.removeError("css");  
-	                    Aria["eval"](res.classDef); 
-	                } else {
+            Aria["eval"](res.classDef); 
+          } else {
 						errors.setError("css", "[SCRIPT ERROR] : " + e.message);
-	                }
-	            }
-	        },{"file_classpath" : "TestStyle"}
-	    );
+          }
+        }
+      },{"file_classpath" : "TestStyle"}
+    );
 	};
 
 	var onEditorChange = function(){
-	    if(!unplugged) {
-	    	// update snippet from editors
-	        snippet[currentType] = editor.getValue();
-	        snippet.data = data_editor.getValue();
+    if(!unplugged) {
+    	// update snippet from editors
+        snippet[currentType] = editor.getValue();
+        snippet.data = data_editor.getValue();
 
-	    	// editors are in sync, just refresh preview
-	        refreshPreview();
-	    }
+    	// editors are in sync, just refresh preview
+        refreshPreview();
+    }
 	};
 
 	// Refresh editors and preview from model
@@ -111,18 +111,18 @@
 	};
 
 	var refreshEditors = function () {
-	    updateEditorSilently(editor, snippet[currentType]);
-	    updateEditorSilently(data_editor, snippet.data);
+    updateEditorSilently(editor, snippet[currentType]);
+    updateEditorSilently(data_editor, snippet.data);
 	};
 
 	var refreshPreview = function () {
 		var data = loadModel(snippet.data);
-	    loadTemplateScript(snippet.script);
-	    loadTemplateStyle(snippet.css, data);  
-	    loadTemplate(snippet.template, data);
+    loadTemplateScript(snippet.script);
+    loadTemplateStyle(snippet.css, data);  
+    loadTemplate(snippet.template, data);
 
-	    aria.templates.TemplateManager.unloadTemplate("Test");
-	    aria.templates.CSSMgr.unloadClassPathDependencies("Test", ["TestStyle"]);
+    aria.templates.TemplateManager.unloadTemplate("Test");
+    aria.templates.CSSMgr.unloadClassPathDependencies("Test", ["TestStyle"]);
 	};
 
 	/**
@@ -141,10 +141,10 @@
 		} else {
 			// addCommand will remove the command before creating the new one
 			editor.commands.addCommand({
-			    name: commandName,
-			    bindKey: {win : winShortcut, mac : macShortcut},
-			    exec: prevCommand.exec,
-			    readOnly: prevCommand.readOnly
+		    name: commandName,
+		    bindKey: {win : winShortcut, mac : macShortcut},
+		    exec: prevCommand.exec,
+		    readOnly: prevCommand.readOnly
 			})
 			return true;
 		}
@@ -253,14 +253,20 @@
 	};
 
 	window.iat = exports;
-	
+	aria.core.AppEnvironment.setEnvironment({
+		defaultWidgetLibs : {
+			"aria" : "aria.widgets.AriaLib", 
+			"html" : "aria.html.HtmlLibrary"
+		}
+	});
+
 	Aria.load({ 
 		classes:["aria.utils.HashManager"], 
-        oncomplete:{
-    	    fn: function () {
-    	    	// loading fake template to get necessary dependencies
-				Aria.loadTemplate({classpath: "A",div: "preview",data:{}}, {fn : init, scope : null});
-    	    }
+      oncomplete:{
+  	    fn: function () {
+  	    	// loading fake template to get necessary dependencies
+					Aria.loadTemplate({classpath: "A",div: "preview",data:{}}, {fn : init, scope : null});
+  	    }
  	    }
     });
 })();
